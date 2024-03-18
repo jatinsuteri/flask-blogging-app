dc62a716ae8050400ba7dfcc6ef5584fcca474dd
Jatin Suteri <jatinsuteri@gmail.com>
2024-03-18 19:59:19 +0530

    changes in register form

--- forms.py
+++ forms.py
@@ -1,17 +1,17 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField,BooleanField
-from wtforms.validators import DataRequired, email, Length,EqualTo
+from wtforms import StringField, PasswordField, SubmitField ,BooleanField
+from wtforms.validators import DataRequired, Email, Length,EqualTo
 
 class RegistrationForm(FlaskForm):
     username = StringField('username', validators=[DataRequired(), Length(min=2,max=20)] )
-    email = StringField('email', validators=[DataRequired(), email()])
-    password = PasswordField('password', validators=[DataRequired() ])
-    confirm_password = PasswordField('confirm_password', validators=[DataRequired(),EqualTo('passowrd')])
-    sumbit = SubmitField('Sign Up')
+    email = StringField('email', validators=[DataRequired(), Email()])
+    password = PasswordField('password', validators=[DataRequired()])
+    confirm_password = PasswordField('confirm_password', validators=[DataRequired(),EqualTo('password')])
+    submit = SubmitField('Sign Up')
     
 class LoginForm(FlaskForm):
     username = StringField('username', validators=[DataRequired(), Length(min=2,max=20)] )
     password = PasswordField('password', validators=[DataRequired() ])
-    submit = SubmitField('Login')
     remember = BooleanField('Remember Me')
+    submit = SubmitField('Login')
     
